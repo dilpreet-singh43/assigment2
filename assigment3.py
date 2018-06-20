560559e698e9c3e39afe6f37b47992cd34cab639
dilpreet-singh43 <dsingh26809@gmail.com>
2018-06-20 14:56:25 +0530

    add the files

---
+++ assigment3.py
@@ -0,0 +1,112 @@
+l=list("mississippi")
+d={}
+d['m']=l.count('m')
+d['i']=l.count('i')
+d['s']=l.count('s')
+d['p']=l.count('p')
+print(d)
+
+
+
+d={}
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+print(d)
+
+
+
+t=(1,4,7,9,4)
+print(max(t))
+print(min(t))
+
+
+
+s1=set([1,2,3,4,5,6])
+s2=set([4,2,3,2,5,7])
+print(s1-s2)
+print(s1&s2)
+print(s)
+
+
+
+d={}
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+name=input("enter your name: ")
+marks=int(input("enter your marks: "))
+d[name]=marks
+    
+print(d)
+values=list(d.values())
+print(values)
+values.sort()
+print(values)
+
+
+t=(1,2,3)
+print(len(t))
+
+t=(3*2*4)
+print(t)
