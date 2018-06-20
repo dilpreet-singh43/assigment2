7bd7fea1f3cb4d44e784fdc84b89803ad915dbf7
dilpreet-singh43 <dsingh26809@gmail.com>
2018-06-20 14:48:37 +0530

    add files

---
+++ Assigment1.py
@@ -0,0 +1,30 @@
+print("learing python")
+print("india")
+
+
+
+print("Dilpreet+Singh")
+print("Best+Friend")
+
+
+
+x=int(input("enter the value of x"))
+y=int(input("enter the value of y"))
+z=(x+y)
+print(z)
+print (type(z))
+
+
+print("let's get started")
+
+
+s="acadview"
+course="python"
+fees=6000
+print("%s,%s,%d"%(s,course,fees))
+
+
+
+name=("Tonny Strak")
+salary=int(1000000)
+print("%s,%d"%(name,salary))
